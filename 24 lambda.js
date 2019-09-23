cc902cb7d3705bc74fc15cbcc1cd77c644e7f0d3
Jacek Kopecký <jacek@jacek.cz>
2019-09-23 10:21:16 +0100

    Update 24 lambda.js

--- 24 lambda.js
+++ 24 lambda.js
@@ -2,8 +2,8 @@ let a = 3;
 console.log( a );
 
 a = function (x,y) { return x+y; };
-//a = (x,y) => { return x+y; };       // shorthand for the above
-//a = (x,y) => x+y;                   // shorthand for the shorthand
+a = (x,y) => { return x+y; };       // shorthand for the above
+a = (x,y) => x+y;                   // shorthand for the shorthand
 
 console.log( a );
 console.log( a( 1,2 ) );
